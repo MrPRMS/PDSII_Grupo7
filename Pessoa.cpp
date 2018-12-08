c39276e3aa7045774527a3b59e434b87691285e1
MrPRMS <paulormsantos@gmail.com>
2018-12-08 20:11:15 -0200

    saco

--- Pessoa.cpp
+++ Pessoa.cpp
@@ -7,16 +7,20 @@ void Pessoa::cadastrar(Pessoa *pessoa)
 	Pessoa::pessoas.insert(std::pair<unsigned int, Pessoa *>(pessoa->get_id(),pessoa));
 }
 
-void Pessoa::deletar()
+void Pessoa::deletar(Pessoa * pessoa)
 {
+	Pessoa::pessoas.erase(pessoa->get_id());
 }
 
-void Pessoa::modificar()
+void Pessoa::modificar(Pessoa *pessoa)
 {
+	std::map<unsigned int, Pessoa *>::iterator it = Pessoa::pessoas.find(pessoa->get_id());
+	it->second=pessoa;
 }
 
-std::vector<Pessoa *> Pessoa::listar()
+std::map<unsigned int, Pessoa *>  Pessoa::listar()
 {
+	return pessoas;
 }
 
 void Pessoa::pesquisar()
