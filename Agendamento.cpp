f94db0cb539fad95c9b8398c30aacefa6239183d
João Paulo Falci Barbosa <37407754+jpfalci@users.noreply.github.com>
2018-12-08 15:16:32 -0200

    Implementação da Classe Agendamento
    Implementação inicial da classe Agendamento.

--- Agendamento.cpp
+++ Agendamento.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <map>
+#include <functional>
+#include <vector>
+#include "Usuario.h"
+#include "Material.h"
 #include "Agendamento.hpp"
 
-int main2(){
+//std::map<unsigned int, Agendamento *> Agendamento::_agendamentos={};
 
-	
-	
-	return 0;
+void Agendamento::cadastrar(Agendamento *agendamento){
+	Agendamento::_agendamentos.insert(std::pair<unsigned int, Agendamento *>(agendamento->get_id(),agendamento));
+}
 
+void Agendamento::deletar(){
+}
+
+void Agendamento::modificar(){
+}
+
+std::vector<Agendamento *> Agendamento::listar(){
+}
+
+void Agendamento::pesquisar(){
 }
