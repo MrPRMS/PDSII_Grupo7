5113ee01c7997a06da00df7d5edd44eac1777f49
João Paulo Falci Barbosa <37407754+jpfalci@users.noreply.github.com>
2018-12-09 21:26:20 -0200

    Atualização Classe Agendamento
    Atualização da Classe Agendamento.

--- Agendamento.cpp
+++ Agendamento.cpp
@@ -7,23 +7,35 @@
 #include "Agendamento.hpp"
 
 Agendamento::Agendamento(){
-	_id++;
+	_id=0;
 	}
 
 std::map<unsigned int, Agendamento *> Agendamento::_agendamentos={};
 
 void Agendamento::cadastrar(Agendamento *agendamento){
+	std::cout<<"Função Agendamento::cadastrar";
 	Agendamento::_agendamentos.insert(std::pair<unsigned int, Agendamento *>(agendamento->get_id(),agendamento));
 }
 
 void Agendamento::deletar(){
+	std::cout<<"Função Agendamento::deletar";
 }
 
 void Agendamento::modificar(Agendamento agendamento){
+	std::cout<<"Função Agendamento::modificar";
 }
 
 std::vector<Agendamento *> Agendamento::listar(){
+	std::cout<<"Função Agendamento::listar";
 }
 
 void Agendamento::pesquisar(){
+	std::cout<<"Função Agendamento::pesquisar";
+}
+
+unsigned int Agendamento::next_id=0;
+
+unsigned int Agendamento::get_new_id(){
+    next_id++;
+    return next_id;
 }
