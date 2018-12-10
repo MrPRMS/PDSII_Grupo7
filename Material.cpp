afd08335f7896bd15d0ec9a30c694d994498a9a2
MrPRMS <paulormsantos@gmail.com>
2018-12-09 23:47:58 -0200

    Criada as classes residuo e alimento, implementado alguns metodos para manter

--- Material.cpp
+++ Material.cpp
@@ -2,10 +2,13 @@
 #include <iostream>
 #include <stdio.h>
 
+unsigned int Material::next_id=0;
+
 Material::Material()
 {
     _codigo=0;
     //ctor
+	id=0;
 }
 
 Material::~Material()
@@ -65,7 +68,7 @@ void Material::PodemSerReciclados(int codigo)
 
     std::cout<<"Metais que podem ser reciclados"<<std::endl;
     std::cout<<"-------------------------------"<<std::endl;
-    std::cout<<"Latas de bebidas e alimentos;tampas de recipientes de vidro; bandejas; panelas; ferragens; grampos; fios eletricos; chapas embalagens de marmitex; cobre;aço; lata de produtos de limpeza."<<std::endl;
+    std::cout<<"Latas de bebidas e alimentos;tampas de recipientes de vidro; bandejas; panelas; ferragens; grampos; fios eletricos; chapas embalagens de marmitex; cobre;aÃ§o; lata de produtos de limpeza."<<std::endl;
     }
     if(codigo==2){
     std::cout<<"Papeis que podem ser reciclados"<<std::endl;
@@ -75,7 +78,7 @@ void Material::PodemSerReciclados(int codigo)
     if(codigo==3){
     std::cout<<"Plasticos que podem ser reciclados"<<std::endl;
     std::cout<<"----------------------------------"<<std::endl;
-    std::cout<<"Embalagens de alimentos, produtos de beleza, produtos de limpeza;tampas; brinquedos; peças plasticas; canetas esferograficas; escovas de dente; baldes; artigos de cozinha."<<std::endl;
+    std::cout<<"Embalagens de alimentos, produtos de beleza, produtos de limpeza;tampas; brinquedos; peÃ§as plasticas; canetas esferograficas; escovas de dente; baldes; artigos de cozinha."<<std::endl;
     }
     if(codigo==4){
     std::cout<<"Vidros que podem ser reciclados"<<std::endl;
@@ -104,7 +107,51 @@ void Material::NaoPodemSerReciclados(int codigo)
     if(codigo==4){
     std::cout<<"Vidros que NAO podem ser reciclados"<<std::endl;
     std::cout<<"-----------------------------------"<<std::endl;
-    std::cout<<"Vidro de automoveis e janelas; pirex; espelho; tubos de TV; lâmpadas; Óculos; ampolas de medicamentos; vidros temperados planos ou de utensílios domésticos"<<std::endl;
+    std::cout<<"Vidro de automoveis e janelas; pirex; espelho; tubos de TV; lÃ¢mpadas; Ã“culos; ampolas de medicamentos; vidros temperados planos ou de utensÃ­lios domÃ©sticos"<<std::endl;
     }
 }
 
+std::map<unsigned int, Material *> Material::materiais={};
+
+void Material::cadastrar(Material *material)
+{
+	Material::materiais.insert(std::pair<unsigned int, Material *>(material->get_id(),material));
+}
+
+void Material::deletar(Material * material)
+{
+	Material::materiais.erase(material->get_id());
+}
+
+void Material::modificar(Material *material)
+{
+	std::map<unsigned int, Material *>::iterator it = Material::materiais.find(material->get_id());
+	it->second=material;
+}
+
+std::map<unsigned int, Material *>  Material::listar()
+{
+	return materiais;
+}
+
+void Material::pesquisar()
+{
+}
+
+std::vector<Material *> Material::pesquisaNome(std::string nome){
+	std::vector<Material *> materiais=std::vector<Material *>();
+	for ( std::map<unsigned int, Material *>::iterator it=Material::materiais.begin(); it!=Material::materiais.end(); ++it){
+		Material* material = dynamic_cast<Material*>(it->second);
+		if(material){
+			if (material->get_nome()==nome)
+				materiais.push_back(material);
+		}
+	}
+	return materiais;
+	
+}
+
+unsigned int Material::get_new_id(){
+    next_id++;
+    return next_id;
+}
